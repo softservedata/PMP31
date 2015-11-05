d11a594d826c36b6e5b927653f57153d7a43c5a0
softservedata <Admin@lab262-pc.franko.lviv.ua>
2015-11-05 16:22:07 +0200

    add Regex

---
+++ RegexExample/RegexExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = "<[^>]*>";
+            //string pattern = ">[^<]*<";
+            string pattern = "(\\w+)(\\.\\w+)*@(\\w+)(.\\w{2,})+";
+            //string invest = "ab12cd234aaa11";
+            //string invest = "<p><b>Beginning with bold text</b> next, text body, <i>italic text</i> end of text.</p>";
+            string invest = "abc._3.a@i.ua";
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Is Response to Pattern: "+regex.IsMatch(invest));
+            MatchCollection collection = regex.Matches(invest);
+            Console.WriteLine("Collection Length = " + collection.Count);
+            if ((collection.Count == 1) && (collection[0].Length == invest.Length))
+            {
+                Console.WriteLine("Full Matches");
+            }
+            foreach (Match match in collection)
+            {
+                int len = match.ToString().Length;
+                //if (len == 2)
+                //{
+                //    continue;
+                //}
+                //Console.WriteLine("\t"+match.ToString().Substring(1, len - 2).Trim());
+                Console.WriteLine("\t" + match.ToString().Trim());
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ RegexExample/RegexExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RegexExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegexExample")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dc26c92-5f56-4e95-8a76-bb31f6e7ba61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
